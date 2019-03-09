c665d574d0eb493229bf43fcc9c7a508e806e7a5
acoshift <acoshift@gmail.com>
2019-03-09 16:12:59 +0700

    change uuid package, init gomod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/acoshift/genuuid
+
+go 1.12
+
+require github.com/gofrs/uuid v3.2.0+incompatible
--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/satori/go.uuid"
+    "github.com/gofrs/uuid"
 )
 
 var (
